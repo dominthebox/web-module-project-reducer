53a2b22ee3bc0f67f3052e0e82e377f52dcbe107
dominthebox <dsallustro@gmail.com>
2021-07-12 22:40:35 -0400

    finished MVP on the project after adding all the memory functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 import reducer, { initialState } from './reducers';
-import { applyNumber, changeOperation } from './actions';
+import { 
+  applyNumber, 
+  changeOperation, 
+  clearDisplay, 
+  addMemory,
+  applyMemory,
+  clearMemory
+ } from './actions';
 
 
 function App() {
@@ -19,6 +26,22 @@ function App() {
 
   const handleOperationClick = (operator) => {
     dispatch(changeOperation(operator));
+  }
+  
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
+  }
+
+  const handleMemoryAdd = () => {
+    dispatch(addMemory());
+  }
+
+  const handleMemoryApply = () => {
+    dispatch(applyMemory());
+  }
+
+  const handleMemoryClear = () => {
+    dispatch(clearMemory());
   }
 
   return (
@@ -38,9 +61,9 @@ function App() {
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton value={"M+"} onClick={handleMemoryAdd}/>
+              <CalcButton value={"MR"} onClick={handleMemoryApply}/>
+              <CalcButton value={"MC"} onClick={handleMemoryClear}/>
             </div>
 
             <div className="row">
@@ -68,7 +91,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={handleClearDisplay}/>
             </div>
 
           </form>
